7ed02fa2d736d0b1b68829e8b1ad3fc55d32aa66
wellington90 <126090580+wellington90@users.noreply.github.com>
2023-10-06 14:10:00 -0300

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -56,7 +56,7 @@ install_docker() {
 
     # Install Docker packages
     sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Add user to docker group
     sudo usermod -aG docker w3ll1n9t0n
