8d8e3a9f9e4a654a50405f217213d7440b2359b2
wellington90 <126090580+wellington90@users.noreply.github.com>
2023-10-09 10:18:48 -0300

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,6 +2,8 @@
 
 #sudo apt-get install gparted
 
+#python 3.10~
+
 #sudo apt install git p7zip-full python3-pip python3-wxgtk4.0 grub2-common grub-pc-bin parted dosfstools ntfs-3g
 #sudo pip3 install WoeUSB-ng
 
